e286ad7589a80f5865be1baa05c17cf9f8f3b9bc
OllieDalli <olliedallipc@gmail.com>
2026-09-17 11:36:44 +0100

    Add current quiz build: Supabase

---
+++ supabase.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://ryijhmvhllurydpdnbhs.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_J4VU-KPurCoHfyxTkJBzEA_jAj0xJLd';
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
